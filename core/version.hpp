b6301a0edf0ae1897c3c75756898d4b072b90a88
luminary-cloud <68704042+luminary-cloud@users.noreply.github.com>
2026-06-10 00:02:44 +0200

    Bump to 1.4.11

--- core/version.hpp
+++ core/version.hpp
@@ -2,7 +2,7 @@
 
 #define SAM_VERSION_MAJOR 1
 #define SAM_VERSION_MINOR 4
-#define SAM_VERSION_PATCH 10
+#define SAM_VERSION_PATCH 11
 
 #ifdef __cplusplus
 
@@ -14,7 +14,7 @@ inline constexpr int kVersionMajor = SAM_VERSION_MAJOR;
 inline constexpr int kVersionMinor = SAM_VERSION_MINOR;
 inline constexpr int kVersionPatch = SAM_VERSION_PATCH;
 
-inline constexpr std::string_view kVersion = "1.4.10";
+inline constexpr std::string_view kVersion = "1.4.11";
 inline constexpr std::string_view kAppName = "Steam Account Manager";
 inline constexpr std::string_view kRepoUrl = "https://github.com/luminary-cloud/steam-account-manager";
 
