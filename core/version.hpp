1617d38363ad87655cc2d92e8796a1bf4e066686
luminary-cloud <68704042+luminary-cloud@users.noreply.github.com>
2026-07-02 12:29:55 +0200

    Bump to 1.8.3

--- core/version.hpp
+++ core/version.hpp
@@ -2,7 +2,7 @@
 
 #define SAM_VERSION_MAJOR 1
 #define SAM_VERSION_MINOR 8
-#define SAM_VERSION_PATCH 2
+#define SAM_VERSION_PATCH 3
 
 #ifdef __cplusplus
 
@@ -14,7 +14,7 @@ inline constexpr int kVersionMajor = SAM_VERSION_MAJOR;
 inline constexpr int kVersionMinor = SAM_VERSION_MINOR;
 inline constexpr int kVersionPatch = SAM_VERSION_PATCH;
 
-inline constexpr std::string_view kVersion = "1.8.2";
+inline constexpr std::string_view kVersion = "1.8.3";
 inline constexpr std::string_view kAppName = "Steam Account Manager";
 inline constexpr std::string_view kRepoUrl = "https://github.com/luminary-cloud/steam-account-manager";
 
